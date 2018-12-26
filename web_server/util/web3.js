bab13d771fb946c34f027c14d077056b0116f9e6
Chiheon Kim <42529723+chiheon@users.noreply.github.com>
2018-12-26 17:06:08 +0900

    Update web3.js

--- web_server/util/web3.js
+++ web_server/util/web3.js
@@ -1,138 +1,90 @@
 const Web3 = require('web3');
 // import Web3 from 'web3';
-const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/ef6f0c88c7214044b408e87817b0f1ae')); // API KEY
-// const web3 = new Web3(new Web3.providers.WebsocketProvider('https://ropsten.etherscan.io/ef6f0c88c7214044b408e87817b0f1ae')); // API KEY
-// const web3 = new Web3(window.web3.currentProvider); // metamask
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/ef6f0c88c7214044b408e87817b0f1ae')); // API KEY
+const web3 = new Web3(new Web3.providers.HttpProvider('http://192.168.43.50:8545')); // API KEY
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); // geth 주소
 
+/* Event listener
+*   event DietData(address indexed party, bytes32 ipfsHash); //party ---> msg.sender라고 생각하면 될 듯
+*   event OfferCreated (address indexed party, uint indexed offerID, string ipfsHash);
+*   event OfferAccepted (address indexed party, uint indexed offerID, string ipfsHash);
+*   event OfferFinalized (address indexed party, uint indexed offerID, string ipfsHash);
+*   event OfferData (address indexed party, uint indexed offerID, string ipfsHash);
+* */
 
 // insert contract data
-const address = '0x723b17a897c770a69f67b256275424530639d3e6';
+const address = '0x254dffcd3277c0b1660f6d42efbb754edababc2b';
 const abi = [
     {
-        "constant": false,
+        "constant": true,
         "inputs": [
             {
-                "name": "ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "addData1",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "listingID",
+                "name": "",
                 "type": "uint256"
             },
-            {
-                "name": "offerID",
-                "type": "uint256"
-            },
-            {
-                "name": "ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "addData2",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "listingID",
-                "type": "uint256"
-            },
-            {
-                "name": "offerID",
-                "type": "uint256"
-            },
-            {
-                "name": "ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "addData3",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "_ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "createListing",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "listingID",
-                "type": "uint256"
-            },
-            {
-                "name": "offerID",
-                "type": "uint256"
-            },
-            {
-                "name": "_ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "finalize",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "listingID",
-                "type": "uint256"
-            },
-            {
-                "name": "_ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "makeOffer",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [],
-        "name": "totalListings",
-        "outputs": [
             {
                 "name": "",
                 "type": "uint256"
             }
         ],
+        "name": "offers",
+        "outputs": [
+            {
+                "name": "buyer",
+                "type": "address"
+            },
+            {
+                "name": "status",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "tokenAddr",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "listings",
+        "outputs": [
+            {
+                "name": "seller",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "name": "_tokenAddr",
+                "type": "address"
+            }
+        ],
         "payable": false,
         "stateMutability": "nonpayable",
-        "type": "function"
+        "type": "constructor"
     },
     {
         "anonymous": false,
@@ -294,103 +246,9 @@ const abi = [
                 "name": "ipfsHash",
                 "type": "string"
             }
-        ],
-        "name": "OfferFinalized",
-        "type": "event"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "name": "party",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "name": "listingID",
-                "type": "uint256"
-            },
-            {
-                "indexed": true,
-                "name": "offerID",
-                "type": "uint256"
-            },
-            {
-                "indexed": false,
-                "name": "ipfsHash",
-                "type": "string"
-            }
-        ],
-        "name": "OfferData",
-        "type": "event"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "listings",
-        "outputs": [
-            {
-                "name": "seller",
-                "type": "address"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            },
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "offers",
-        "outputs": [
-            {
-                "name": "buyer",
-                "type": "address"
-            },
-            {
-                "name": "status",
-                "type": "uint8"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "name": "listingID",
-                "type": "uint256"
-            }
-        ],
-        "name": "totalOffers",
-        "outputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
+        ]
     }
-];
+    ];
 
 var contractInstance = new web3.eth.Contract(abi,address);
 
